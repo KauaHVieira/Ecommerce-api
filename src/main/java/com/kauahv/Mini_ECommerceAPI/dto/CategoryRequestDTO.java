6ffddc320884f690bbbb2f08f87b33d7f231872d
Kaua Henrique <kauahv7@gmail.com>
2026-04-06 18:44:13 -0300

    feature(Category): add Category request and response DTOs

---
+++ src/main/java/com/kauahv/Mini_ECommerceAPI/dto/CategoryRequestDTO.java
@@ -0,0 +1,26 @@
+package com.kauahv.Mini_ECommerceAPI.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryRequestDTO {
+
+    @NotBlank
+    private String name;
+    private String description;
+    private UUID categoryParentId;
+
+
+}
