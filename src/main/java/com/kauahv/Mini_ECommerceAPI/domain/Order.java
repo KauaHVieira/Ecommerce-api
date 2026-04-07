6fc9d1f3adb264737f214cfb4fa180b42477e4c0
Kaua Henrique <kauahv7@gmail.com>
2026-04-07 19:10:04 -0300

    feature(Order): add entity order

---
+++ src/main/java/com/kauahv/Mini_ECommerceAPI/domain/Order.java
@@ -0,0 +1,34 @@
+package com.kauahv.Mini_ECommerceAPI.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @EqualsAndHashCode.Include
+    private UUID id;
+    private Instant moment;
+    @ManyToOne
+    private User client;
+    private Integer orderStatus;
+    //private Payment payment;
+    //private Set<OrderItem> items = new HashSet<>();
+
+}
