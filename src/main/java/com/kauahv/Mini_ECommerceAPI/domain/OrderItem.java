0a54e73b9e872b8224b8f170b47fe5d06c795ef8
Kaua Henrique <kauahv7@gmail.com>
2026-04-14 18:06:17 -0300

    feature(OrderItem): add OrderItem entity and add OrderItemPK

---
+++ src/main/java/com/kauahv/Mini_ECommerceAPI/domain/OrderItem.java
@@ -0,0 +1,30 @@
+package com.kauahv.Mini_ECommerceAPI.domain;
+
+import com.kauahv.Mini_ECommerceAPI.domain.pk.OrderItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+public class OrderItem {
+
+    @EmbeddedId
+    private OrderItemPK id = new OrderItemPK();
+    private BigDecimal price;
+    private Integer quantity;
+
+    public Order getOrder(){ return id.getOrder(); }
+
+    public void setOrder(Order order){ id.setOrder(order); }
+
+    public Product getProduct(){ return id.getProduct(); }
+
+    public void setProduct(Product product){ id.setProduct(product); }
+
+    public BigDecimal getSubTotal(){
+        return price.multiply(BigDecimal.valueOf(quantity));
+    }
+}
