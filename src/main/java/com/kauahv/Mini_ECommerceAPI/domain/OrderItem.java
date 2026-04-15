519be3e9305369d5e48bd4c4c6085a489903c1c7
Kaua Henrique <kauahv7@gmail.com>
2026-04-15 18:11:10 -0300

    feature(OrderItem): add OrderItem mapper

--- src/main/java/com/kauahv/Mini_ECommerceAPI/domain/OrderItem.java
+++ src/main/java/com/kauahv/Mini_ECommerceAPI/domain/OrderItem.java
@@ -5,10 +5,18 @@ import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "order_items")
 public class OrderItem {
